b4deaa31fad30354c21c5cd1bf55c8abf4563e53
ren-hh <98929676+ren-hh@users.noreply.github.com>
2022-02-09 22:15:18 -0500

    made frog objects in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,71 @@
-//these are listeners to respond to button clicks
+//these are frog objects containing photo and description.
 
+var frogObject1 = {
+	image:"<img src='https://live.staticflickr.com/7249/13255580745_5803be80b1_b.jpg'/>",
+	description: "<p> Dendrobates auratus (Green and Black Poison Dart Frog) is the largest poison-dart frog in Costa Rica, with adult females ranging from 27.0-42.0 mm and adult males ranging from 25.0-39.5 mm (Leenders 2001). It is native to Central America, from the Caribbean slopes of southern Nicaragua to extreme eastern Panama, and on the Pacific slopes from the Osa Peninsula, Costa Rica to Colombia (Guyer and Donnelly 2005). It lives in lowlands, primarily rainforest (Caldwell and Summers 2003). It is a shy species, found deep in the forest interior (Leenders 2001). In 1932, it was introduced to Oahu, Hawaii, as a means for mosquito control, and continues to occur there (McKeown 1978, 1996). </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Dendrobates&where-species=auratus'>  Visit Amphibia Web to learn more </a></p>"
+}
+
+var frogObject2 = {
+	image:"<img src='https://ssl.c.photoshelter.com/img-get/I0000yzcNXEL9wZE/s/880/880/AH-Lovely-Poison-Frog-Phyllobates-lugubris-Costa-Rica-8497.jpg'/>",
+	description: "<p> Phyllobates lugubris ('Striped Poison Dart Frog') occurs in the Caribbean lowlands, from the extreme southeastern tip of Nicaragua through Costa Rica and into northwestern Panama.  It has been found in leaf litter in both primary and secondary forests, preferring to live near slow-moving waters (Guyer and Donnelly 2005). This species exhibits uniparental male care (Weygoldt 1987; Zimmermann and Zimmermann 1988). After the female deposits the eggs in a ground nest, they are periodically moistened by the male parent (hydric brooding) (Savage 2002; Guyer and Donnelly 2005). Once the eggs are hatched, the male transports his offspring to aquatic rearing sites by carrying five to ten tadpoles at a time on his back (Donnelly et al. 1990; Caldwell et al. 1994). In about two months they metamorphose into froglets of about 12 mm (Savage 2002). </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Phyllobates&where-species=lugubris'>Visit Amphibia Web to learn more</a></p>"
+}
+
+var frogObject3 = {
+	image: "<img src='https://photos.willbl.com/wp-content/uploads/WBL-Costa-Rica-Frog-110601215207.jpg'/>",
+	description: " <p> Oophaga pumilio ('Strawberry Dart Frog') is found in the rainforests of the Caribbean coast of Central America, from Nicaragua to Panama. Males defend territories, approximately 3 m apart from other males (Walls 1994). The female approaches the male to initiate breeding. It is the role of the male to tend to the terrestrial clutch of eggs (3-17 eggs), and to keep them moist by periodically emptying their bladders on the eggs (hydric brooding) until they hatch (after 5-15 days). Males may protect more than one clutch at a time. The female then carries the individual tadpoles (1-2) to separate, water filled leaf axils of bromeliads or other plants. The female lays unfertilized eggs in with the tadpoles to serve as a food source. Adults of this species primarily consume ants. </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Oophaga&where-species=pumilio '>Visit Amphibia Web to learn more</a></p>"
+}
+
+var frogObject4 = {
+		image:"<img src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/strawberry-poison-dart-frog-la-selva-field-station-costa-rica-phil-savoie--natureplcom.jpg'/>",
+		description: "<p> The Blue Jeans frog is just a different phenotype of the Strawberry frog. This species appears in a wide range of colors, and scientists are still developing theories as to what drives the coloration patterns and population divergence.  <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Oophaga&where-species=pumilio '>Visit Amphibia Web to learn more</a></p>"
+}
+
+var frogObject5 = {
+	image: "<img src='http://www.escapingabroad.com/blog/wp-content/uploads/2014/11/golfo-dulce-poison-dart-frog2_lrg.jpg'/>",
+	description: "<p> Phyllobates vittatus ('Golfo Dulce Poison-Dart Frog') is endemic to Costa Rica, inhabiting the wet forests and lowlands of the Golfo Dulce region in the southwestern part of Costa Rica (Silverstone 1976; Leenders 2001). It prefers the leaf litter of forested valleys with streams (Leenders 2001). This species has parental care, with the male parent attending the clutch of eggs up to three times a day and engaging in hydric brooding (Savage 2002).A few days to a week after the eggs hatch, the male will allow some or all of his tadpoles to crawl up on his back (Savage 2002). He then transports from one to thirteen larvae at a time, carrying them for 1-2 days, to a water source.  </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Phyllobates&where-species=vittatus '>Visit Amphibia Web to learn more</a></p>"
+}
+
+var frogObject6 = {
+	image: "<img src='https://ssl.c.photoshelter.com/img-get/I0000nXvfmfI5rFg/s/880/880/AH-Granular-Poison-Frog-Oophaga-granulifera-3827.jpg'/>",
+	description: "<p>Oophaga granulifera ('Granular Poison Dart Frog') occurs mainly in southwestern Costa Rica, on low mountains near Puntarenas. This species is highly territorial (Goodman 1971; Crump 1972; Bolaños 1990; van Wijngaarden and van Gool 1994), showing male-male physical aggression (Goodman 1971; Crump 1972). Territoriality in this species involves defending calling sites and oviposition sites (Bolaños 1990), and is vocally mediated (van Wijngaarden and van Gool 1994). </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Oophaga&where-species=granulifera'>Visit Amphibia Web to learn more</a></p>"
+}
+
+
+// thses are listeners to respond to button clicks
 $('#frog1').on('click', function() {
-	$(".photo").html("<img src='https://live.staticflickr.com/7249/13255580745_5803be80b1_b.jpg'/> <p> Dendrobates auratus ('Green and Black Poison Dart Frog') is the largest poison-dart frog in Costa Rica, with adult females ranging from 27.0-42.0 mm and adult males ranging from 25.0-39.5 mm (Leenders 2001). It is native to Central America, from the Caribbean slopes of southern Nicaragua to extreme eastern Panama, and on the Pacific slopes from the Osa Peninsula, Costa Rica to Colombia (Guyer and Donnelly 2005). It lives in lowlands, primarily rainforest (Caldwell and Summers 2003). It is a shy species, found deep in the forest interior (Leenders 2001). In 1932, it was introduced to Oahu, Hawaii, as a means for mosquito control, and continues to occur there (McKeown 1978, 1996).</p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Dendrobates&where-species=auratus'> Visit Amphibia Web to learn more</a></p>");
-})
 
-// var frogObject1 = {
-// 	image:'https://live.staticflickr.com/7249/13255580745_5803be80b1_b.jpg'
-// 	description: "<p> Dendrobates auratus (Green and Black Poison Dart Frog) is the largest poison-dart frog in Costa Rica, with adult females ranging from 27.0-42.0 mm and adult males ranging from 25.0-39.5 mm (Leenders 2001). It is native to Central America, from the Caribbean slopes of southern Nicaragua to extreme eastern Panama, and on the Pacific slopes from the Osa Peninsula, Costa Rica to Colombia (Guyer and Donnelly 2005). It lives in lowlands, primarily rainforest (Caldwell and Summers 2003). It is a shy species, found deep in the forest interior (Leenders 2001). In 1932, it was introduced to Oahu, Hawaii, as a means for mosquito control, and continues to occur there (McKeown 1978, 1996). </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Dendrobates&where-species=auratus'>  Visit Amphibia Web to learn more </a></p>"
-//
-// }
-//
-// console.log(frogObject1.image, frogObject1.description)
-//
-// $('#frog1').on('click', function() {
-//
-// 	// $('#content-space').image('frog11.image')
-// 	$('#content-space').html('frog11.description')
-//
-// })
+	$("#content-space").html(frogObject1.image)
+	$('#content-space').append(frogObject1.description)
+})
 
 $('#frog2').on('click', function() {
-	$(".photo").html("<img src='https://ssl.c.photoshelter.com/img-get/I0000yzcNXEL9wZE/s/880/880/AH-Lovely-Poison-Frog-Phyllobates-lugubris-Costa-Rica-8497.jpg'/> <p> Phyllobates lugubris ('Striped Poison Dart Frog') occurs in the Caribbean lowlands, from the extreme southeastern tip of Nicaragua through Costa Rica and into northwestern Panama.  It has been found in leaf litter in both primary and secondary forests, preferring to live near slow-moving waters (Guyer and Donnelly 2005). This species exhibits uniparental male care (Weygoldt 1987; Zimmermann and Zimmermann 1988). After the female deposits the eggs in a ground nest, they are periodically moistened by the male parent (hydric brooding) (Savage 2002; Guyer and Donnelly 2005). Once the eggs are hatched, the male transports his offspring to aquatic rearing sites by carrying five to ten tadpoles at a time on his back (Donnelly et al. 1990; Caldwell et al. 1994). In about two months they metamorphose into froglets of about 12 mm (Savage 2002). </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Phyllobates&where-species=lugubris'>Visit Amphibia Web to learn more</a></p>");
 
+	$("#content-space").html(frogObject2.image)
+	$('#content-space').append(frogObject2.description)
 })
+
 
 $('#frog3').on('click', function() {
-	$(".photo").html("<img src='https://photos.willbl.com/wp-content/uploads/WBL-Costa-Rica-Frog-110601215207.jpg'/> <p> Oophaga pumilio ('Strawberry Dart Frog') is found in the rainforests of the Caribbean coast of Central America, from Nicaragua to Panama. Males defend territories, approximately 3 m apart from other males (Walls 1994). The female approaches the male to initiate breeding. It is the role of the male to tend to the terrestrial clutch of eggs (3-17 eggs), and to keep them moist by periodically emptying their bladders on the eggs (hydric brooding) until they hatch (after 5-15 days). Males may protect more than one clutch at a time. The female then carries the individual tadpoles (1-2) to separate, water filled leaf axils of bromeliads or other plants. The female lays unfertilized eggs in with the tadpoles to serve as a food source. Adults of this species primarily consume ants. </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Oophaga&where-species=pumilio '>Visit Amphibia Web to learn more</a></p>");
+
+	$("#content-space").html(frogObject3.image)
+	$('#content-space').append(frogObject3.description)
 })
 
+
 $('#frog4').on('click', function() {
-	$(".photo").html("<img src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/strawberry-poison-dart-frog-la-selva-field-station-costa-rica-phil-savoie--natureplcom.jpg'/><p>  </p> The Blue Jeans frog is just a different phenotype of the Strawberry frog. This species appears in a wide range of colors, and scientists are still developing theories as to what drives the coloration patterns and population divergence.  <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Oophaga&where-species=pumilio '>Visit Amphibia Web to learn more</a></p>");
+
+	$("#content-space").html(frogObject4.image)
+	$('#content-space').append(frogObject4.description)
 })
 
 $('#frog5').on('click', function() {
-	$(".photo").html("<img src='http://www.escapingabroad.com/blog/wp-content/uploads/2014/11/golfo-dulce-poison-dart-frog2_lrg.jpg'/> <p> Phyllobates vittatus ('Golfo Dulce Poison-Dart Frog') is endemic to Costa Rica, inhabiting the wet forests and lowlands of the Golfo Dulce region in the southwestern part of Costa Rica (Silverstone 1976; Leenders 2001). It prefers the leaf litter of forested valleys with streams (Leenders 2001). This species has parental care, with the male parent attending the clutch of eggs up to three times a day and engaging in hydric brooding (Savage 2002).A few days to a week after the eggs hatch, the male will allow some or all of his tadpoles to crawl up on his back (Savage 2002). He then transports from one to thirteen larvae at a time, carrying them for 1-2 days, to a water source.  </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Phyllobates&where-species=vittatus '>Visit Amphibia Web to learn more</a></p>")
+
+	$("#content-space").html(frogObject5.image)
+	$('#content-space').append(frogObject5.description)
 })
 
 $('#frog6').on('click', function() {
-	$(".photo").html("<img src='https://ssl.c.photoshelter.com/img-get/I0000nXvfmfI5rFg/s/880/880/AH-Granular-Poison-Frog-Oophaga-granulifera-3827.jpg'/> <p>Oophaga granulifera ('Granular Poison Dart Frog') occurs mainly in southwestern Costa Rica, on low mountains near Puntarenas. This species is highly territorial (Goodman 1971; Crump 1972; Bolaños 1990; van Wijngaarden and van Gool 1994), showing male-male physical aggression (Goodman 1971; Crump 1972). Territoriality in this species involves defending calling sites and oviposition sites (Bolaños 1990), and is vocally mediated (van Wijngaarden and van Gool 1994). </p> <p> <a href='https://amphibiaweb.org/cgi/amphib_query?where-genus=Oophaga&where-species=granulifera'>Visit Amphibia Web to learn more</a></p>");
+
+	$("#content-space").html(frogObject6.image)
+	$('#content-space').append(frogObject6.description)
 })
